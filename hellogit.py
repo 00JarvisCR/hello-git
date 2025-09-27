6e4b77b2e5d71c94bcc568852c33d48bc5877c41
Jeremy <ing.jeremy.quiel@gmail.com>
2025-09-27 09:01:51 -0600

    se le agrega un bulce a cada codigo

--- hellogit.py
+++ hellogit.py
@@ -1,1 +1,3 @@
 print(f"Hello Git")
+for i in range(0, 99999):
+    print(i)
--- hellogit2.java
+++ hellogit2.java
@@ -1,5 +1,8 @@
 public class hellogit2 {
     public static void main(String[] args) {
         System.out.println("hello git 2");
+        for(int i = 0; i < 99999; i++) {
+            System.out.println(i);
+        }
     }
 }
